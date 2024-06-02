f6c11608ac88b0cdc964d52fd3292790bff53abb
kyuraa <abub89025@gmail.com>
2024-06-02 14:49:42 +0800

    First Commit

---
+++ resources/views/component/sidebar.blade.php
@@ -0,0 +1,52 @@
+<section id="sidebar">
+		<a href="#" class="brand">
+			<i class='bx bxs-laugh'></i>
+			<span class="text">STARTiT</span>
+		</a>
+		<ul class="side-menu top">
+			<li id="button-dashboard" class="active">
+				<a href="#">
+					<i class='bx bxs-dashboard' ></i>
+					<span class="text">Home</span>
+				</a>
+			</li>
+			<li id="button-usaha">
+				<a href="#">
+					<i class='bx bxs-shopping-bag-alt' ></i>
+					<span class="text">Business</span>
+				</a>
+			</li>
+			<li id="button-analytics">
+				<a href="#">
+					<i class='bx bxs-doughnut-chart' ></i>
+					<span class="text">Analytics</span>
+				</a>
+			</li>
+			<li id="button-message">
+				<a href="#">
+					<i class='bx bxs-message-dots' ></i>
+					<span class="text">Message</span>
+				</a>
+			</li>
+			<li id="button-trend">
+				<a href="#">
+					<i class='bx bx-trending-up' ></i>
+					<span class="text">Trend Pasar</span>
+				</a>
+			</li>
+		</ul>
+		<ul class="side-menu">
+			<li id="button-setting">
+				<a href="#">
+					<i class='bx bxs-cog' ></i>
+					<span class="text">Settings</span>
+				</a>
+			</li>
+			<li id="button-logout">
+				<a href="logout.php" class="logout">
+					<i class='bx bxs-log-out-circle' ></i>
+					<span class="text">Logout</span>
+				</a>
+			</li>
+		</ul>
+	</section>
